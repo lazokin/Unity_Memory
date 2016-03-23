b7559037b7092f72e7d9a72a1982a4ce6cadab75
Nikolce Ambukovski <lazokin@hotmail.com>
2016-03-23 15:34:37 +1100

    Displaying the various card images

--- Assets/Scripts/MemoryCard.cs
+++ Assets/Scripts/MemoryCard.cs
@@ -4,6 +4,18 @@ using System.Collections;
 public class MemoryCard : MonoBehaviour {
 
     [SerializeField] private GameObject cardBack;
+    [SerializeField] private SceneController conotroller;
+
+    private int _id;
+
+    public int id {
+        get { return _id; }
+    }
+
+    public void SetCard(int id, Sprite image) {
+        _id = id;
+        GetComponent<SpriteRenderer>().sprite = image;
+    }
 
     public void OnMouseDown() {
         if (cardBack.activeSelf) {
