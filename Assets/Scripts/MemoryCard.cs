dfc65f26238f911fb461f8d6aee678b2c4bd8387
Nikolce Ambukovski <lazokin@hotmail.com>
2016-03-23 15:09:44 +1100

    Building a card object and making it react to clicks

---
+++ Assets/Scripts/MemoryCard.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MemoryCard : MonoBehaviour {
+
+    [SerializeField] private GameObject cardBack;
+
+    public void OnMouseDown() {
+        if (cardBack.activeSelf) {
+            cardBack.SetActive(false);
+        }
+    }
+
+}
